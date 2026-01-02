c52701fc417eedb35b2e0f4b359168139086800c
homow <homowsh897@gmail.com>
2026-01-02 03:59:16 +0330

    feat(user-util) add a userUtil for safing user data

---
+++ src/lib/utils/userUtils.ts
@@ -0,0 +1,12 @@
+import type {UserDB} from "@src/types/index.js";
+
+export function getSafeUser(data: UserDB) {
+    return {
+        id: data._id.toString(),
+        name: data.name,
+        email: data.email,
+        age: data.age,
+        createdAt: data.createdAt.toISOString(),
+        updatedAt: data.updatedAt.toISOString(),
+    };
+}
