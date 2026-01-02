ba1a31ba72e00df249d866e36085f87242477220
homow <homowsh897@gmail.com>
2026-01-02 08:56:09 +0330

    refactor(UserRole) refactor user model with UserRole

--- src/lib/utils/userUtils.ts
+++ src/lib/utils/userUtils.ts
@@ -3,6 +3,7 @@ import type {UserDB} from "@src/types/index.js";
 export function getSafeUser(data: UserDB) {
     return {
         id: data._id.toString(),
+        role: data.role,
         name: data.name,
         email: data.email,
         age: data.age,
