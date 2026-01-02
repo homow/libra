316df248033855650ee57f5082d99575c2e0aca7
homow <homowsh897@gmail.com>
2026-01-02 05:18:08 +0330

    add(service) add new service for signupUser

---
+++ src/services/usersService/signupUserService.ts
@@ -0,0 +1,33 @@
+import type {UserInput} from "@src/validtaion/user.js";
+import UserModel from "@src/models/User.js";
+import {getSafeUser} from "@lib/utils/userUtils.js";
+import type {ServiceReturnDataType} from "@src/types/index.js";
+
+export default async function signupUserService(
+    body: UserInput
+): Promise<ServiceReturnDataType> {
+    const user = await UserModel.findOne({
+        email: body.email,
+    });
+
+    if (user) {
+        return {
+            status: 409,
+            data: {
+                ok: false,
+                message: "User already exists",
+            }
+        };
+    }
+
+    const newUser = await UserModel.create(body);
+
+    return {
+        status: 201,
+        data: {
+            ok: true,
+            message: "User created successfully",
+            user: getSafeUser(newUser),
+        }
+    };
+}
