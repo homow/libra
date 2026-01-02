cb71fe5337280d61dcbefa273c2d86928e0a77b7
homow <homowsh897@gmail.com>
2026-01-02 06:49:23 +0330

    refactor(user) refactor user with controller and services

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,8 @@
-import UserModel from "@src/models/User.js";
-import {getSafeUser} from "@lib/utils/userUtils.js";
-import {validateBody} from "@middleware/paresBody.js";
-import {internalServerError} from "@lib/api/response.js";
-import express, {type Request, type Response} from 'express';
-import validateObjectId from "@middleware/validateObjectId.js";
+import express from 'express';
 import {UserSchema} from "@src/validtaion/user.js";
+import {validateBody} from "@middleware/paresBody.js";
+import validateObjectId from "@middleware/validateObjectId.js";
+import deleteUserController from "@controllers/usersController/deleteUserController.js";
 import signupUserController from "@controllers/usersController/signupUserController.js";
 
 const usersRouter = express.Router();
@@ -16,30 +14,7 @@ usersRouter.post('/signup',
 
 usersRouter.delete('/:id',
     validateObjectId(),
-    async (req: Request<{ id: string }>, res: Response) => {
-        const id = req.params.id;
-
-        try {
-            const findUser = await UserModel.findOne({_id: id});
-
-            if (!findUser) {
-                return res.status(404).json({
-                    ok: false,
-                    message: "User does not exist",
-                });
-            }
-
-            await UserModel.deleteOne({_id: id});
-
-            res.status(200).json({
-                ok: true,
-                message: "User deleted successfully",
-                user: getSafeUser(findUser),
-            });
-        } catch (_) {
-            return internalServerError(res);
-        }
-    }
+    deleteUserController
 );
 
 export default usersRouter;
