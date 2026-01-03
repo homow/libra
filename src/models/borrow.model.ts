c624e1968fc4ee5e4d9c8a77e74692d8f52ddec9
homow <homowsh897@gmail.com>
2026-01-03 05:49:57 +0330

    feat(Model) create a mongo Model for borrow books

---
+++ src/models/borrow.model.ts
@@ -0,0 +1,45 @@
+import type {BorrowDB} from "@src/types/models/borrow.types.js";
+import mongoose, {Types, type Schema, type Model} from "mongoose";
+
+const BorrowModelSchema: Schema<BorrowDB> = new mongoose.Schema({
+    bookId: {
+        type: Types.ObjectId,
+        ref: "Book",
+    },
+    userId: {
+        type: Types.ObjectId,
+        ref: "User",
+    },
+    borrowedAt: {
+        type: Date,
+        default: Date.now,
+    },
+    isReturned: {
+        type: Boolean,
+        default: false,
+    },
+    returnedAt: {
+        type: Date,
+        default: null,
+    },
+}, {
+    timestamps: true
+});
+
+BorrowModelSchema.pre("save", async function () {
+    if (this.returnedAt && !this.isReturned) {
+        this.isReturned = true;
+    } else if (!this.returnedAt && this.isReturned) {
+        this.returnedAt = new Date();
+    }
+});
+
+BorrowModelSchema.index(
+    {bookId: 1, userId: 1, isReturned: 1}
+);
+
+const BorrowModel: Model<BorrowDB> =
+    mongoose.models.Borrow
+    || mongoose.model<BorrowDB>("Book", BorrowModelSchema);
+
+export default BorrowModel;
